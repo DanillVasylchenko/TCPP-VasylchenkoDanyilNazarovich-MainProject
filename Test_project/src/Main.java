3ae8f058226edb5e25a75a6c6f5ce0be56958de2
DanillVasylchenko <danillvasychenko@gmail.com>
2021-02-26 19:32:48 +0200

    Зміни_2

--- Test_project/src/Main.java
+++ Test_project/src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args){
         System.out.println("k");
         System.out.println("ALPHA");
+        System.out.println("ALPHA_2");
     }
 }
